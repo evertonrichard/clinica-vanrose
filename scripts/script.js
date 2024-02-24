d0e98fb18e3ff73710b8ca0925ee25764635c2bc
evertonrichard <evertonrichard444@gmail.com>
2024-02-24 15:07:26 -0300

    Revert "terminado o formulario IMC"
    This reverts commit 7fe006219613a8448021c7698267640deddfc0ae.

--- scripts/script.js
+++ scripts/script.js
@@ -1,130 +1,75 @@
-//IMC
-//1.Pegar os valores 
-//2.Calcular o IMC
-//3.Gerar a classificação do IMC
-//4.Organizar as informações
-//5.Salvar os dados na lista 
-//6.Ler a lista com os dados
-//7.Renderizar o conteúdo no HTML (tabela)
-//8.Botão de limpar os registros
+//imc - ok
+//1 capturar os valores - ok
+//2 caucular o imc - ok
+//3 gerar a clacificação do imc - ok
+//4 organizar as informações - ok
+//5 salvar os dados em uma lista (array)
+//6 ler a lista com os dados(array)  -> localStorage
+//7 enderizar o conteúdo na tela.
+//botão limpar registros -> clear localstorage
 
-function calcularImc(event) {
-    event.preventDefault()
-
-    let dadosUsuario = pegarValores();
-
-    let imc = calcular(dadosUsuario.altura, dadosUsuario.peso);
-
-    let classificacaoImc = classificarImc(imc);
-
-    let usuarioAtt = organizarDados(dadosUsuario, imc, classificacaoImc);
-
-    cadastrarUsuario(usuarioAtt);
-
-    window.location.reload();
-}
-
+// função receber os valores dos inputs e coverter o mesmo a um objeto 
 function pegarValores() {
-    let nome = document.getElementById("nome").value.trim();
-    let altura = parseFloat(document.getElementById("altura").value);
-    let peso = parseFloat(document.getElementById("peso").value);
+    let nome = document.getElementById('nome').value.trim();
+    let altura = parseFloat(document.getElementById('altura').value)
+    let peso = parseFloat(document.getElementById('peso').value)
 
-    let dadosUsuario = {
-        nome: nome,
-        altura: altura,
-        peso: peso
+    //variaveis do objeto recebendo os dados do front-end 
+
+    let dadosUsuarios = {
+        nomeUsuario: nome,
+        alturaUsuario: altura,
+        pesoUsuario: peso
+
     }
-
-    return dadosUsuario;
+    return
+    
 }
 
-function calcular(altura, peso) {
-    let imc = peso / (altura * altura);
+function caucular (altura, peso) {
+    let imc = peso / (altura * altura)
 
-    return imc;
+    return
+
 }
 
-function classificarImc(imc) {
-    if (imc < 18.5) {
-        return "Abaixo do Normal"
+function classificacaoImc (imc) {
 
+    if (imc < 18 ) {
+        return "abaixo do peso "
     } else if (imc < 25) {
-        return "Normal"
-
-    } else if (imc < 30) {
-        return "Sobrepeso"
-
+        return "peso nomral"
+    } else if (imc < 30 ){
+        return "sobrepeso"
+    } else if (imc < 35) {
+        return "obesidade 1"
     } else {
-        return "Obesidade"
+        return " obesidade 2 e 3"
     }
 }
 
-function organizarDados(dadosUsuario, valorImc, classificacaoImc) {
-    let dataHoraAtual = Intl.DateTimeFormat('pt-BR', { timeStyle: 'long', dateStyle: 'short' }).format(Date.now());
+function organizarDados(dadosUsuarios, valorImc, classificacaoImc){
+    let dataAtual = Intl.DateTimeFormat('pt-BR', {timeStyle: 'long', dateStyle: 'short'}).format(Date.now());
     let dadosUsuarioAtt = {
-        ...dadosUsuario,
+        ...dadosUsuarios,
         imc: valorImc.toFixed(2),
         classificacao: classificacaoImc,
-        dataCadastro: dataHoraAtual
+        dataCadastro : dataAtual
     }
-
-    console.log(dadosUsuarioAtt);
-
     return dadosUsuarioAtt;
 }
 
+//funcção responsavél por armazenar o objeto dento do local storage
 function cadastrarUsuario(usuario) {
-    let listaUsuarios = [];
+    let listaDeUsuario = []
+//validação para verificar se eu tenho algum dado dentro do local storage do navegador
+    if (localStorage.getItem("usuariosCadastrados")){
+        listaDeUsuario = JSON.parse(localStorage.getItem("usuariosCadastrados"))
 
-    // if (localStorage.getItem("usuariosCadastrados") == true) padrao do if
-    if (localStorage.getItem("usuariosCadastrados")) {
-        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
     }
 
-    listaUsuarios.push(usuario)
+    listaDeUsuario.pesoUsuario(usuario)
 
-    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
-}
+    localStorage.setItem("usuarioCadastrados", JSON.stringify(listaDeUsuario))
 
-function carregarUsuarios() {
-    let listaUsuarios = [];
-
-    if (localStorage.getItem("usuariosCadastrados")) {
-        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
-    }
-
-    if (listaUsuarios.length == 0) {
-        let tabela = document.getElementById("corpo-tabela");
-
-        tabela.innerHTML = `<tr class="linha-mensagem">
-            <td colspan="6">Nenhum usuário cadastrado !</td>
-        </tr>`
-    } else {
-        montarTabela(listaUsuarios);
-    }
-}
-
-window.addEventListener('DOMContentLoaded', () => carregarUsuarios());
-
-function montarTabela(listaDeCadastrados) {
-    let tabela = document.getElementById("corpo-tabela");
-
-   
-    listaDeCadastrados.forEach(pessoa => {
-        tabela.innerHTML += `<tr>
-            <td data-cell="nome">${pessoa.nome}</td>
-            <td data-cell="altura">${pessoa.altura}</td>
-            <td data-cell="peso">${pessoa.peso}</td>
-            <td data-cell="valor do IMC">${pessoa.imc}</td>
-            <td data-cell="classificação do IMC">${pessoa.classificacao}</td>
-            <td data-cell="data de cadastro">${pessoa.dataCadastro}</td>
-        </tr>`
-    });
-
-   
-}
-
-function deletarRegistros() {
-    localStorage.removeItem("usuariosCadastrados")
-    window.location.reload();
 }
